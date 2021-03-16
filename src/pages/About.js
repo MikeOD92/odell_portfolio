b75d24d5ceafaef8e5b71bbb51724c025cfa69ba
Michael <michaelodell660@gmail.com>
2021-03-16 15:12:56 -0700

    starting home page design and layout

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,15 +4,7 @@ export default function About(props) {
 	return (
 		<div className="AboutPage">
 			<h3> About me</h3>
-			<p>
-				{' '}
-				am a software engineer and a multimedia artist, with a strong DIY ethos.{' '}
-			</p>
-			<p>
-				{' '}
-				I use code, creative softwares and other tools to bring unique visual
-				experiences to life.
-			</p>
+			<p> I am a software engineer and a multimedia artist. </p>
 		</div>
 	);
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,13 @@ import React, { useState } from 'react';
 export default function App(props) {
 	return (
 		<div className="HomePage">
+			<header class="header-bar">
+				<h1> Michael T. O'Dell</h1>
+			</header>
 			<div id="splash-container">
-				<h1> Michael O'Dell</h1>
-
 				<div id="red-texture"></div>
 				<div id="blue-box"></div>
-
-				<h3> Web developer / Software Engineer / Artist</h3>
+				<h3> Web Developer / Software Engineer / Artist</h3>
 			</div>
 		</div>
 	);
